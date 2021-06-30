0ed4df3e0e8e1589ee6eea536b1d1e402bb23cee
cesar villa <cvillaf@hotmail.com>
2021-06-29 17:01:57 -0700

    intento de modularizacion de productos

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,6 +1,11 @@
+const fs = require('fs');
+
 const controlador = {
+    
     index: (req, res) => {
-        res.render('index');
+        let productosFile = fs.readFileSync('./database/productos.json', 'utf8');
+        let productos = JSON.parse(productosFile);
+        res.render('index', {'productos': productos});
     },
     productDetail: (req, res) => {
         res.render('productDetail');
