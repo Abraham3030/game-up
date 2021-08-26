629073601ac356821ecfb412ce9529f0986e83ae
Abraham Montelongo <mocapunk@hotmail.com>
2021-08-26 01:40:37 -0500

    configuracion base de datos heroku

--- database/config/config.js
+++ database/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": "",
+    "username": "b3e60ac61c4d8a",
+    "password": "4849df3b",
     "database": "heroku_ff97a4ef30cc0e6",
-    "host": "127.0.0.1",
+    "host": "us-cdbr-east-04.cleardb.com",
     "dialect": "mysql",
     "port":"3306"
   },
