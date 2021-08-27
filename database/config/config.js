d648b9fdf69c85fc9ec6b8a2acbee95875567cf7
Abraham Montelongo <mocapunk@hotmail.com>
2021-08-26 21:03:41 -0500

    configuracion config}

--- database/config/config.js
+++ database/config/config.js
@@ -7,17 +7,17 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": "b3e60ac61c4d8a",
+    "password": "4849df3b",
+    "database": "heroku_ff97a4ef30cc0e6",
+    "host": "us-cdbr-east-04.cleardb.com",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "b3e60ac61c4d8a",
+    "password": "4849df3b",
+    "database": "heroku_ff97a4ef30cc0e6",
+    "host": "us-cdbr-east-04.cleardb.com",
     "dialect": "mysql"
   }
 }
